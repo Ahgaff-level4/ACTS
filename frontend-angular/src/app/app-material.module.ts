bc71f2d81faac06ba3f77eccc5a2030dc7d70230
Ahmad Alkaf <Ahmad.Alkaf.AHK@gmail.com>
2023-03-31 19:23:11 +0300

    add

--- frontend-angular/src/app/app-material.module.ts
+++ frontend-angular/src/app/app-material.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
-import {MatAutocompleteModule} from '@angular/material/autocomplete'
-import {MatButtonModule} from '@angular/material/button'
+import { MatAutocompleteModule } from '@angular/material/autocomplete'
+import { MatButtonModule } from '@angular/material/button'
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatIconModule } from '@angular/material/icon'
 import { MatCheckboxModule } from '@angular/material/checkbox'
-import {MatDialogModule} from '@angular/material/dialog'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatTableModule } from '@angular/material/table';
+
 @NgModule({
-  exports:[
+  exports: [
     MatAutocompleteModule,
     MatButtonModule,
     MatInputModule,
@@ -16,7 +18,9 @@ import {MatDialogModule} from '@angular/material/dialog'
     MatToolbarModule,
     MatIconModule,
     MatCheckboxModule,
-    MatDialogModule
+    MatDialogModule,
+    MatTableModule
   ]
 })
-export class AppMaterialModule { }
+export class AppMaterialModule {
+}
