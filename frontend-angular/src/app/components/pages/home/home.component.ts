3f5b44ce0d1e53b923b07f7d2c6434ab01aeee52
Ahmad Alkaf <Ahmad.Alkaf.AHK@gmail.com>
2023-05-30 17:50:31 +0300

    fix home's cards base on user role

--- frontend-angular/src/app/components/pages/home/home.component.ts
+++ frontend-angular/src/app/components/pages/home/home.component.ts
@@ -1,31 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UtilityService } from 'src/app/services/utility.service';
+import { Role } from '../../../../../../interfaces';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   constructor(public ut: UtilityService) { }
 
-  public cards:{title:string, link:string,img:string,alt?:string,desc:string}[] = [
-    {title:'Children', link:'/children',img:'assets/img/girl.svg',alt:'girl photo',desc:'Children information'},
-    {title:'Settings', link:'/settings',img:'assets/img/Setting.svg',desc:'Children information'},
-    {title:'Accounts', link:'/accounts',img:'assets/img/Account.svg',desc:'Accounts information page access their information and add/edit or event delete be caution your actions have results'},
+  public cards!: Card[];
 
+  ngOnInit(): void {
+    const _cards: Card[] = [
+      {
+        title: 'Children',
+        link: '/children',
+        img: 'assets/img/girl.svg', alt: 'girl photo',
+        desc: 'Children information',
+        role: ['Admin', 'HeadOfDepartment', 'Teacher', 'Parent',]
+      },
+      {
+        title: "Special Activities",
+        img: "assets/img/Activity.svg",
+        link: "/special-activities",
+        desc: "Special Activities information",
+        role: ['Admin', 'HeadOfDepartment']
+      },
+      {
+        title: "Programs",
+        img: "assets/img/Program.svg",
+        link: "/program",
+        desc: "Programs information",
+        role: ['Admin', 'HeadOfDepartment', 'Teacher']
+      },
+      {
+        title: 'Accounts',
+        link: '/accounts',
+        img: 'assets/img/Account.svg',
+        desc: 'Manage all users accounts',
+        role: ['Admin']
+      },
+      {
+        title: 'Settings',
+        link: '/settings',
+        img: 'assets/img/Setting.svg',
+        desc: 'Settings and preference',
+      },
 
-    {
-      "title": "Activities",
-      "img": "assets/img/Activity.svg",
-      "link":"adsf",
-      "desc": "In voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-    },
-    {
-      "title": "Programs",
-      "img": "assets/img/Program.svg",
-      "link":"adsf",
-      "desc": "Sunt in culpa qui officia deserunt mollit anim id est laborum."
-    }
-  ]
+    ];
+
+    this.cards = _cards.filter(v => v.role ? this.ut.userHasAny(...v.role) : true);
+  }
+}
+
+interface Card {
+  title: string, link: string, img: string, alt?: string, desc: string, role?: Role[]
 }
