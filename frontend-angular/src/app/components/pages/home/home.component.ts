760361e0251d098a2f4454c4215518e054588984
Abdullah <Abdullah.mashi2015@gmail.com>
2023-05-30 16:52:02 +0300

    add the card icon, and make them hover

--- frontend-angular/src/app/components/pages/home/home.component.ts
+++ frontend-angular/src/app/components/pages/home/home.component.ts
@@ -11,29 +11,19 @@ export class HomeComponent {
 
   public cards:{title:string, link:string,img:string,alt?:string,desc:string}[] = [
     {title:'Children', link:'/children',img:'assets/img/girl.svg',alt:'girl photo',desc:'Children information'},
-    {title:'Settings', link:'/settings',img:'assets/app-UI/Home page – 1.svg',desc:'Children information'},
-    {title:'Accounts', link:'/accounts',img:'assets/img/favicon.ico',desc:'Accounts information page access their information and add/edit or event delete be caution your actions have results'},
+    {title:'Settings', link:'/settings',img:'assets/img/Setting.svg',desc:'Children information'},
+    {title:'Accounts', link:'/accounts',img:'assets/img/Account.svg',desc:'Accounts information page access their information and add/edit or event delete be caution your actions have results'},
+
+
     {
-      "title": "Lorem ipsum dolor sit amet",
-      "img": "assets/img/girl.svg",
-      "link":"adsf",
-      "desc": "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },
-    {
-      "title": "Ut enim ad minim veniam",
-      "img": "assets/img/girl.svg",
-      "link":"adsf",
-      "desc": "Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    },
-    {
-      "title": "Duis aute irure dolor in reprehenderit",
-      "img": "assets/img/girl.svg",
+      "title": "Activities",
+      "img": "assets/img/Activity.svg",
       "link":"adsf",
       "desc": "In voluptate velit esse cillum dolore eu fugiat nulla pariatur."
     },
     {
-      "title": "Excepteur sint occaecat cupidatat non proident",
-      "img": "assets/img/girl.svg",
+      "title": "Programs",
+      "img": "assets/img/Program.svg",
       "link":"adsf",
       "desc": "Sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
