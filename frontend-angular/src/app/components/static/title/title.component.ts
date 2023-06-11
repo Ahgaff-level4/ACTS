c071a10b404aff5517757c5382a12d911dd75c76
Ahmad Alkaf <Ahmad.Alkaf.AHK@gmail.com>
2023-06-11 16:52:51 +0300

    added feature to app-title to force providing properties

--- frontend-angular/src/app/components/static/title/title.component.ts
+++ frontend-angular/src/app/components/static/title/title.component.ts
@@ -2,13 +2,14 @@ import { Component, Input } from '@angular/core';
 import { UtilityService } from 'src/app/services/utility.service';
 
 @Component({
-  selector: 'app-title',
+  selector: 'app-title[title1][isPrinting]',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })/**
-Shared component to set the title for any page (No need for translation). Ex: `title1=Goals`, `title2=Goals of`: `param2=Ahmad`... */
+Shared component to set the title for any page (No need for translation). Ex: `title1=Goals`, `title2=Goals of`: `param2=Ahmad`...
+Note: `title1` and `isPrinting` are required*/
 export class TitleComponent {
-  @Input('title1') title1: string | undefined;
+  @Input('title1') title1: string = '';
   @Input('title2') title2: string | undefined;
   @Input('param2') param2: string | undefined;
   @Input('title3') title3: string | undefined;
