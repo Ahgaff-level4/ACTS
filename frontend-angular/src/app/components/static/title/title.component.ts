54d1254d0988ed423903de3089f7c2ca420adfa5
Ahmad Alkaf <Ahmad.Alkaf.AHK@gmail.com>
2023-06-15 01:21:58 +0300

    changed title comp. to array and looks like file path

--- frontend-angular/src/app/components/static/title/title.component.ts
+++ frontend-angular/src/app/components/static/title/title.component.ts
@@ -1,23 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { UtilityService } from 'src/app/services/utility.service';
 
 @Component({
-  selector: 'app-title[title1][isPrinting]',
+  selector: 'app-title[header][isPrinting]',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })/**
 Shared component to set the title for any page (No need for translation). Ex: `title1=Goals`, `title2=Goals of`: `param2=Ahmad`...
 Note: `title1` and `isPrinting` are required*/
-export class TitleComponent {
-  @Input('title1') title1: string = '';
-  @Input('title2') title2: string | undefined;
-  @Input('param2') param2: string | undefined;
-  @Input('title3') title3: string | undefined;
-  @Input('param3') param3: string | undefined;
-  @Input('backLink') backLink: string | undefined;
-  @Input('backTooltip') backTooltip: string | undefined;
+export class TitleComponent implements OnInit {
+  @Input('header') title: string = '';/**title is preserve word for browser tooltip */
+  @Input() links: Link[] = [];
   /**Hide back button when printing */
   @Input('isPrinting') isPrinting: boolean | undefined;
 
+  protected back: Link | undefined;
   constructor(public ut: UtilityService) { }
+  ngOnInit(): void {
+    this.back = this.links[this.links.length - 2];
+  }
+
+  /**Add "Page" prefix to the link's title. (e.g. 'Page title_name') */
+  tooltipOf(link: Link) {
+    return this.ut.getDirection() == 'rtl' ?
+      this.ut.translate('page') + ' ' + this.ut.translate(link.title) + (link.param ? ' ' + link.param : '')
+      : (link.title) + (link.param ? ' ' + link.param : '') + ' ' + this.ut.translate('page');
+  }
+}
+
+interface Link {
+  title: string,
+  param?: string,
+  link?: string
 }
