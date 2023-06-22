43d6d06df98d1da5f660f133ff176e2ba14e7b6f
Ahmad Alkaf <Ahmad.Alkaf.AHK@gmail.com>
2023-06-22 11:55:07 +0300

    done send notification message UI

---
+++ frontend-angular/src/app/components/dialogs/notification-drawer/send-message/send-message.component.ts
@@ -0,0 +1,33 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FormService } from 'src/app/services/form.service';
+import { NotificationService, OnlineAccount } from 'src/app/services/notification.service';
+
+@Component({
+  selector: 'app-send-message',
+  templateUrl: './send-message.component.html',
+  styleUrls: ['./send-message.component.scss']
+})
+export class SendMessageComponent implements OnInit {
+  public formGroup = this.fb.group({
+    message: [null, [Validators.required, Validators.minLength(3)]],
+  });
+  constructor(private dialogRef: MatDialogRef<any>, private fb: FormBuilder,
+    public nt: NotificationService,
+    @Inject(MAT_DIALOG_DATA) public account?: OnlineAccount) { }
+
+  ngOnInit(): void {
+    if (!this.account && !this.nt.onlineAccounts.value) {
+      this.nt.notify(undefined);
+      this.dialogRef.close();
+    }
+  }
+  send() {
+    this.formGroup.disable();
+    //todo some code
+    this.dialogRef.close();
+    //todo some code
+    this.formGroup.enable();
+  }
+}
