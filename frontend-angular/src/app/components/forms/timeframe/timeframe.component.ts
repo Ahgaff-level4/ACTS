ebf6265a286eb18533ea25b759aed949ec2d651e
Ahmad Alkaf <Ahmad.Alkaf.AHK@gmail.com>
2023-07-16 00:12:44 +0300

    refactor rounded button

--- frontend-angular/src/app/components/forms/timeframe/timeframe.component.ts
+++ frontend-angular/src/app/components/forms/timeframe/timeframe.component.ts
@@ -25,18 +25,17 @@ export class TimeframeComponent {
     } else if (t == 'Yearly') {
       this.timeframeFormGroup.controls.from.setValue(new Date(this.now.getFullYear() - 1, this.now.getMonth(), this.now.getDate()));
       this.timeframeFormGroup.controls.to.setValue(this.now);
-    }else if(t=='Monthly'){
+    } else if (t == 'Monthly') {
       this.timeframeFormGroup.controls.from.setValue(new Date(this.now.getFullYear(), this.now.getMonth() - 1, this.now.getDate()));
       this.timeframeFormGroup.controls.to.setValue(this.now);
-    }else if(t=='Weekly'){
+    } else if (t == 'Weekly') {
       this.timeframeFormGroup.controls.from.setValue(new Date(this.now.getTime() - 7 * 24 * 60 * 60 * 1000));
       this.timeframeFormGroup.controls.to.setValue(this.now);
     }
-    // this.picker10.stateChanges.next()
-    // this.picker10.datepickerInput.getStartValue
-    // this.picker10.select(moment.duration({ from: moment(new Date(0)), to: moment(new Date()) }))
-    //   this.picker10.startAt = moment.duration({ from: moment(new Date(0)), to: moment(new Date()) });
-    //   this.picker10.at
-    //   console.log(this.picker10.datepickerInput.getStartValue())
+  }
+
+  chosenTimeframe(): TimeframeDuration | '' {
+    //todo add hint of the chosen timeframe if not custom
+    return '';
   }
 }
